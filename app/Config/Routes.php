9f8a7e2585621eba0912f90f015387bf720fa1a1
Lucas Spitzer <104842840+spitzerl@users.noreply.github.com>
2024-10-17 14:17:35 +0200

    Installation dépendences (#1)
    * adding json dependencies
    
    * removing wrong dependencies
    
    * adding bootstrap
    
    Adding bootstrap through composer
    
    * Revert "adding bootstrap"
    
    This reverts commit 073999f7fa0a70405a9ee7fb404f3c02997c31da.
    
    * modifying dependencies
    
    * completed bootstrap sass install

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('compile/scss_to_css', 'CompilerV2::scss_to_css');
+$routes->get('test', 'TestController::index');
---
+++ app/Controllers/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class TestController extends BaseController
+{
+    public function index()
+    {
+        return "Ceci est une page de test!";
+    }
+}
