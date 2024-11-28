36c6efed3fe91d9849168a89506f8b1ab982189b
Azmog <80716388+AzmogEx@users.noreply.github.com>
2024-11-28 08:29:31 +0100

    modif navbar

--- app/Views/components/navbar.php
+++ app/Views/components/navbar.php
@@ -10,17 +10,17 @@
 <!--FIN CALENDAR GOOGLE-->
 <nav class="navbar navbar-expand-lg bg-flowtech">
     <div class="container-fluid">
-        <a class="navbar-brand magz text-dark" href="/">FlowTech</a>
+        <a class="navbar-brand magz text-white" href="/">FlowTech</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="/#services">Nos services</a>
+                    <a class="nav-link text-white" href="/#services">Nos services</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/#contact">Contact</a>
+                    <a class="nav-link text-white" href="/#contact">Contact</a>
                 </li>
             </ul>
             <div class="d-flex" role="search">
